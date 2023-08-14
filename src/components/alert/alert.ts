bb508796e9ee3e46d9d45ec4acc4dd349bb4043e
mariohamann <mario.hamann@virtual-identity.com>
2023-08-15 00:30:18 +0200

    feat: remove automatic files

--- src/components/alert/alert.ts
+++
@@ -1,5 +0,0 @@
-// SYNCED FILE – REMOVING THIS LINE WILL NOT SYNC IT ANYMORE
-import LdAlert from './alert.component.js';
-export * from './alert.component.js';
-export default LdAlert;
-LdAlert.define('ld-alert');
--- src/components/icon/icon.ts
+++
@@ -1,5 +0,0 @@
-// SYNCED FILE – REMOVING THIS LINE WILL NOT SYNC IT ANYMORE
-import LdIcon from './icon.component.js';
-export * from './icon.component.js';
-export default LdIcon;
-LdIcon.define('ld-icon');
